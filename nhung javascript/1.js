6d08e86750463d927142a75c78c0ca02af7c3918
hoang van luan <luan29793@gmail.com>
2019-09-06 11:36:05 +0700

    lan4

---
+++ nhung javascript/1.js
@@ -0,0 +1,6 @@
+
+<button onclick="document.getElementById('myImage').src='taoanh.gif'">Ảnh 1</button>
+  <img id="myImage" src="taoanh.gif" style="width:500px">
+  <button onclick="document.getElementById('myImage').src='baibbien.gif'">Ảnh 2</button>
+  <button onclick="document.getElementById('myImage').src='chonban.gif'">Ảnh 3</button>
+}
